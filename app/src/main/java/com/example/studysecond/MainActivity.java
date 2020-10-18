6d4f4a18cb24f5745bd1a30a32a15f1469ac0a57
youngchang <yychang.liu@gmail.com>
2020-10-18 22:56:59 +0900

    Initial commit

---
+++ app/src/main/java/com/example/studysecond/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.studysecond;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
+        super.onStart();
+    }
+
+    private void updateUI(GoogleSignInAccount account) {
+        Toast.makeText(getApplicationContext(),"이미 가입된 계정입니다 로그인을 해주세요",Toast.LENGTH_LONG).show();
+        return;
+    }
+}
---
+++ app/src/main/java/com/example/studysecond/SignActivity.java
@@ -0,0 +1,28 @@
+package com.example.studysecond;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
+public class SignActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // Build a GoogleSignInClient with the options specified by gso.
+
+       GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this,gso);
+
+    }
+}
